7068256ec9addbfd95147140f552f303befd0b40
NikolaiOD <110899590+NikolaiOD@users.noreply.github.com>
2023-02-10 16:19:51 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # http-requests-newsapi
+HTTP-запросы
--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,8 @@ function onSubmit(e) {
 
   API.getNews(inputValue)
     .then(({ articles }) => {
-      if (articles.length === 0) throw new Error("No data");
+      // if (articles.length === 0) throw new Error("No data");
+      if (inputValue === '') throw new Error("No data");
 
       return articles.reduce(
         (markup, article) => createMarkup(article) + markup,
